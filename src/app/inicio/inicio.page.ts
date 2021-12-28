10388f7014e6e9dad84de4cdfa5af5d7b2066443
LuisCastDev <90583642+LuisCastDev@users.noreply.github.com>
2021-12-28 00:42:20 -0400

    Trabajando en el counter

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
 import { ServiciosService } from '../servicios.service';
 
 @Component({
@@ -7,13 +8,37 @@ import { ServiciosService } from '../servicios.service';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+  productosTotal : any;
+  clientesTotal: any;
+  producto: string = '';
+
+
 
   constructor(
-    public servicios: ServiciosService
-  ) { }
+    public servicios: ServiciosService,
+    public toast: ToastController,
+    public loading: LoadingController
+  ) {   this.setCounter(); }
 
   ngOnInit() {
   }
+  ionViewWillEnter(){
+    this.setCounter();
+  }
+
+ async setCounter(){
+   let l = await this.loading.create();
+   l.present();
+
+    this.servicios.Producto_Listado(this.producto).subscribe((data:any)=>{
+      this.productosTotal = data.info.totalActivo;
+      console.log(this.productosTotal);
+      l.dismiss();
+    },(error:any)=>{
+      console.log(error);
+      l.dismiss();
+    });
 
 
+  }
 }
